5d38e51adf43edecc1e4be32d64a2fd790540bdc
ABODJI-Kondi-Kaled <kondi.kaled.abodji@gmail.com>
2025-04-30 17:21:27 +0100

    Adding Liskov Subtitution principle example

--- SOLID_philosophy_exemples/part1_example.py
+++ SOLID_philosophy_exemples/part1_example.py
@@ -62,5 +62,42 @@ class StudentDiscount(Discount):
     def apply(self, price):
         return price * 0.85
 # 3- Liskov Substitution Principle (LSP)
-
+# ❌ Bad Practice: A class that violates LSP
+class Bird:
+    def fly(self):
+        print("Flying")
+class Ostrich(Bird): # Ostrich is a bird but cannot fly
+    def fly(self):
+        raise Exception("Ostriches can't fly, they run instead")
+class Penguin(Bird): # Penguin is a bird but cannot fly
+    def fly(self):
+        raise Exception("Penguins can't fly, it swims instead")
+    
+# ✅ Good Practice: A class that adheres to LSP
+class Bird:
+    def move(self):
+        print("Moving")
+class Sparrow(Bird):
+    def move(self):
+        print("Flying")
+class Ostrich(Bird):
+    def move(self):
+        print("Running")
+class Penguin(Bird):
+    def move(self):
+        print("Swimming")
+               
+# 4- Interface Segregation Principle (ISP)
+# ❌ Bad Practice: A class that violates ISP
+class Printer:
+    def print(self, document):
+        print(f"Printing {document}")
+    def scan(self, document):
+        print(f"Scanning {document}")
+    def fax(self, document):
+        print(f"Faxing {document}")
+# ✅ Good Practice: Separate interfaces for different functionalities
+class Printer:
+    def print(self, document):
+        print(f"Printing {document}")
 
